0bde14d3de470269c31bbfccc6b5fddd9d0b266f
el cheddarmma <fdrios81@gmail.com>
2026-05-08 01:20:38 -0300

    Agregamos a cors el origen permitido de nuestro front

--- src/server.js
+++ src/server.js
@@ -8,7 +8,11 @@ dotenv.config()
 const app = express()
 
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: [
+    "http://localhost:5173",
+    "https://celadon-torte-02d9bc.netlify.app/",
+
+    ],
 }))
 
 app.use(express.json())
